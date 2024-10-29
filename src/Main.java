40e8dbb6ea777057d471afcfeb64abb6d0e968c3
Mehrbod Molla Kazemi <98255802+Mehrbod-MK@users.noreply.github.com>
2024-10-29 20:25:27 +0330

    Print some welcome messages

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("In the name of God");
+        System.out.println("Guess the number Game");
+        System.out.println("Developer:  Mehrbod Molla Kazemi");
+        System.out.println("Date:  29 October 2024");
+
+
+    }
+}
